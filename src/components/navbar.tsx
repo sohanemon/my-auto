77f8d1d526bbcae2c378a9391402d53ac29f51ce
sohanemon <m.sohanemon@gmail.com>
2023-06-07 11:13:50 +0600

    feat: navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,3 +1,12 @@
+'use client';
+import { ReactSVG } from 'react-svg';
+
 export default function Navbar() {
-  return <nav className='max-w-5xl'></nav>;
+  return (
+    <nav className='bg-white'>
+      <div className='max-w-5xl p-4 mx-auto '>
+        <ReactSVG src='/assets/logo.svg' />
+      </div>
+    </nav>
+  );
 }
