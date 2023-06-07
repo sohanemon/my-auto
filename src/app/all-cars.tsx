9a7947540fa5dc315222a6736e47f140ff6d2fe5
sohanemon <m.sohanemon@gmail.com>
2023-06-07 13:36:30 +0600

    feat: nav space bottom

--- src/app/all-cars.tsx
+++ src/app/all-cars.tsx
@@ -3,7 +3,7 @@ import CarCard from './car-card';
 
 export default function AllCars() {
   return (
-    <section className='col-span-3'>
+    <section className='col-span-3 space-y-3'>
       <CarCard />
       <CarCard />
     </section>
--- src/app/car-card.tsx
+++ src/app/car-card.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export default function CarCard() {
   return (
-    <section className='flex p-4 bg-white rounded-2xl'>
+    <section className='flex gap-4 p-4 bg-white rounded-2xl'>
       <Image
         className='rounded-lg'
         src={'/assets/car.png'}
@@ -12,17 +12,18 @@ export default function CarCard() {
       />
       <div className='grow'>
         <div className='flex items-center justify-between'>
-          <div className='flex items-center'>
+          <div className='flex items-center gap-2'>
             <p className='text-sm font-bold text-themeBlack'>
               LAND ROVER Range Rover Evoque
             </p>
             <p className='text-sm text-gray-400 font-75bold'>2013 წ</p>
           </div>
-          <div className='flex items-center '>
+          <div className='flex items-center gap-4'>
             <p className='text-xs font-bold leading-3 text-themeRed'>
               განბაჟება
             </p>
             <div className='w-2 h-2 bg-yellow-400 rounded-full' />
+            <p className='text-xs text-gray-500 font-75bold'>აშშ</p>
           </div>
         </div>
       </div>
