ba667beb320f8e828dee79fe8c4a7c6498b9793a
sohanemon <m.sohanemon@gmail.com>
2023-06-07 13:00:20 +0600

    feat: sidebar

---
+++ src/app/all-cars.tsx
@@ -0,0 +1,10 @@
+export default function AllCars() {
+  return (
+    <section className='col-span-3'>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi voluptate
+      odit itaque cumque corporis ex expedita, saepe recusandae nemo fugit
+      ducimus harum adipisci corrupti nobis laboriosam suscipit mollitia debitis
+      aliquid?
+    </section>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
+import AllCars from './all-cars';
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main className='grid max-w-5xl grid-cols-4 mx-auto'>
+      {/* left side */}
+      <div />
+      {/* right side */}
+      <AllCars />
+    </main>
+  );
 }
