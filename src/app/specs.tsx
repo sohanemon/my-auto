781cf5128f043a8a46b7b5d3ccdcd379d4e78b7f
sohanemon <m.sohanemon@gmail.com>
2023-06-09 10:07:31 +0000

    Jun 9, 2023, 4:07 PM

--- src/app/specs.tsx
+++ src/app/specs.tsx
@@ -11,7 +11,7 @@ export default function Specs({ icon, text }: SpecType) {
   return (
     <>
       <div className='flex items-center gap-3 basis-1/2'>
-        <ReactSVG src={icon} />
+        <ReactSVG src={icon} className='max-sm:hidden' />
         <p className='text-sm font-semibold text-themeBlack'>{text}</p>
       </div>
     </>
