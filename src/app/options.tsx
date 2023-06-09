c7a74e70b93424040443a4d6b55e3e57a498d01f
sohanemon <m.sohanemon@gmail.com>
2023-06-09 09:35:01 +0000

    Jun 9, 2023, 3:35 PM

--- src/app/options.tsx
+++ src/app/options.tsx
@@ -4,7 +4,8 @@ import { ReactSVG } from 'react-svg';
 export default function Options() {
   return (
     <div className='flex items-center gap-4'>
-      {option.map(({ icon }) => {
+      {option.map(({ icon, mobileOnly }) => {
+        if (mobileOnly) return;
         return <ReactSVG src={icon} key={icon} />;
       })}
     </div>
