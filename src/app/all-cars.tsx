b60acabd5e3ae63f5462d931450a34335c108ffa
sohanemon <m.sohanemon@gmail.com>
2023-06-09 07:19:38 +0600

    feat: sorting

--- src/app/all-cars.tsx
+++ src/app/all-cars.tsx
@@ -1,17 +1,36 @@
 'use client';
 import useCars from '@/store/cars';
+import useFilter from '@/store/filter';
+import { Suspense, useMemo } from 'react';
 import AllCarsHeader from './all-cars-header';
 import CarCard from './car-card';
-import { Suspense, useEffect, useLayoutEffect } from 'react';
 
 export default function AllCars() {
-  const cars = useCars((s) => s.cars);
-
+  const cars: CarData[] = useCars((s) => s.cars);
+  const sortingType = useFilter((s) => s.sortingType);
+  const filteredCar: CarData[] = useMemo(() => {
+    switch (sortingType) {
+      // price dec
+      case 'ფასი კლებადი':
+        return cars.sort((a, b) => b.price - a.price);
+      // price inc
+      case 'ფასი ზრდადი':
+        return cars.sort((a, b) => a.price - b.price);
+      // run inc
+      case 'გარბენი კლებადი':
+        return cars.sort((a, b) => a.car_run_km - b.car_run_km);
+      // run dec
+      case 'გარბენი ზრდადი':
+        return cars.sort((a, b) => b.car_run_km - a.car_run_km);
+      default:
+        return cars;
+    }
+  }, [cars, sortingType]);
   return (
     <section className='col-span-3 space-y-3'>
       {/* @ts-ignore */}
       <AllCarsHeader />
-      {cars?.map(
+      {filteredCar?.map(
         (
           car: CarData // @ts-ignore
         ) => (
