4afbd2e67beea4f61270087dda9d1f3c7483c245
sohanemon <m.sohanemon@gmail.com>
2023-06-09 13:50:50 +0000

    Jun 9, 2023, 7:50 PM

--- src/store/filter.ts
+++ src/store/filter.ts
@@ -33,7 +33,7 @@ const store: StoreType = (set: Function, get: Function) => ({
     const res = await fetch(
       `https://api2.myauto.ge/ka/products/?Period=${
         get().selectedPeriod
-      }&SortOrder=${get().selectedSorting}`
+      }&SortOrder=${get().selectedSorting}&Cats=${get().selectedCategory}`
     );
     const data = await res.json();
     console.log('🛑 ~ getCars ~ data:', data);
