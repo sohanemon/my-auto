03c14ae1f6156671d6b5607788e1080fc7c75f59
sohanemon <m.sohanemon@gmail.com>
2023-06-09 14:11:04 +0000

    Jun 9, 2023, 8:11 PM

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 // @ts-nocheck
+import Loader from '@/components/loader';
 import AllCars from './all-cars';
 import Filters from './filters';
 
 export default function Home() {
   return (
     <main className='grid max-w-5xl grid-cols-4 mx-auto sm:p-4'>
+      <Loader />
       {/* left side */}
       <Filters />
       {/* right side */}
