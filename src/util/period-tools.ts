e0ca52b3dda5c2d0143af82aab1982e499a57bf7
sohanemon <m.sohanemon@gmail.com>
2023-06-09 10:33:54 +0000

    Jun 9, 2023, 4:33 PM

--- src/util/period-tools.ts
+++ src/util/period-tools.ts
@@ -1,7 +1,12 @@
 // @ts-nocheck
-export function isHourOld(date: string, hour: number) {
-  var hourInMilliseconds = hour * 60 * 60 * 1000;
-  var currentDate = new Date();
-  var parsedOrderDate = new Date(date);
+export function isHourOld(date: string, hour: string) {
+  let multiplier;
+  if (hour.includes('h')) multiplier = hour[0];
+  else if (hour.includes('d')) multiplier = hour[0] * 24;
+  else multiplier = hour[0] * 24 * 7;
+
+  const hourInMilliseconds = multiplier * 60 * 60 * 1000;
+  const currentDate = new Date();
+  const parsedOrderDate = new Date(date);
   return currentDate - parsedOrderDate >= hourInMilliseconds;
 }
