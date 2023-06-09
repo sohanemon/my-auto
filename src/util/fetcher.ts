efadcf6bb9404355d88b346953d2756f7694724a
sohanemon <m.sohanemon@gmail.com>
2023-06-09 13:41:36 +0000

    Jun 9, 2023, 7:41 PM

--- src/util/fetcher.ts
+++ src/util/fetcher.ts
@@ -1,3 +1,5 @@
+// @ts-ignore
+export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export async function manFetcher() {
   const res = await fetch('https://static.my.ge/myauto/js/mans.json');
   return await res.json();
